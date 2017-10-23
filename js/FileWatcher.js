9eb6f8e47f1b9010d5c1298f72e3b5c9fabedb18
cement <32463636@qq.com>
2017-10-23 18:08:57 +0800

    add a filewatcher file

---
+++ js/FileWatcher.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const { URL } = require('url');
+const url = new URL('file:///L:/FileWatchTest/');
+
+fs.watch('/', {persistent:true,recursive:true,encoding:'utf8'}, function(eventType,fileName){
+    console.log(eventType);
+    console.log(fileName);
+    var res = fs.readFileSync(fileName, {encoding:'utf8'})
+    console.log(res);
+})
+
+fs.watchFile('./test.html', (curr, prev) => {
+  console.log(`the current mtime is: ${curr.mtime}`);
+  console.log(`the previous mtime was: ${prev.mtime}`);
+});
+
+// fs.watch('./watch.html', {persistent:true,recursive:true,encoding:'utf8'}, function(eventType,fileName){
+//     console.log(eventType);
+//     console.log(fileName);
+
+// })
